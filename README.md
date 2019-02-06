529a2839b407a30e6324ea35bbede37e89faf659
mdesson <michael.desson@gmail.com>
2019-02-06 09:29:42 -0500

    Added config file, updated readme to reflect this

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ On Windows, use:
 ```
 SET DEBUG=locallibrary:* & npm run devstart
 ```
+
+## Configuring
+
+You must have a config.json in your root directory.
+
+Your Mongoose database user's username and password are stored as "user" and "pass", respectively.
--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Import config file
+var rawConfig = fs.readFileSync('config.json');
+var config = JSON.parse(rawConfig);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
