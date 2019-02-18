2c944a86f1b2ca35a1898dd14de7098eef314c9f
mdesson <michael.desson@gmail.com>
2019-02-18 11:10:39 -0500

    Finished bookintstance controller and template. Not working

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,6 @@
 var Book = require('../models/book');
 var Author = require('../models/author');
-var Genre = require('../models/author');
+var Genre = require('../models/genre');
 var BookInstance = require('../models/bookinstance');
 var async = require('async');
 
